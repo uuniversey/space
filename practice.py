e10a743f5d908dda45cae690c69acb2a081a4227
unknown <mm9326@naver.com>
2023-07-15 00:06:07 +0900

    perfect

--- practice.py
+++ practice.py
@@ -1,18 +1,16 @@
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         key = arr[i]
+#         j = i - 1
+#         while j >= 0 and arr[j] > key:
+#             arr[j + 1] = arr[j]
+#             j -= 1
+#         arr[j + 1] = key
 
-data = [5, 2, 4, 6, 1, 3]
-insertion_sort(data)
-print(data)  # 출력: [1, 2, 3, 4, 5, 6]
-
-
+# data = [5, 2, 4, 6, 1, 3]
+# insertion_sort(data)
+# print(data)  # 출력: [1, 2, 3, 4, 5, 6]
 
 
 
@@ -30,21 +28,16 @@ print(data)  # 출력: [1, 2, 3, 4, 5, 6]
 
 
 
-# cases = int(input())
-
-# box = [input()]
-# data = box[0].split(' ')
-# clear = sorted(data)
-
-# for a in range(cases-1):
-#     if (len(clear)) % 2 == 1:
-#         clear.pop(0)
-#     else:
-#         clear.pop()
-
-# print(clear)
 
 
+cases = int(input())
+
+box = input().split()
+result = sorted(map(int, box))
+
+num = len(result) // 2
+
+print (result[num])
 
 
 
