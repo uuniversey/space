bb099c24c7e8bd3cdbed836dd8d1e20cb9f479e1
LWJ <mm9326@naver.com>
2023-07-14 17:29:55 +0900

    self study

--- practice.py
+++ practice.py
@@ -1,12 +1,48 @@
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+data = [5, 2, 4, 6, 1, 3]
+insertion_sort(data)
+print(data)  # 출력: [1, 2, 3, 4, 5, 6]
 
 
-box = ['1', '3', '5', '7', '100000']
-ans = max(int(box))
-
-print(ans)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cases = int(input())
+
+# box = [input()]
+# data = box[0].split(' ')
+# clear = sorted(data)
+
+# for a in range(cases-1):
+#     if (len(clear)) % 2 == 1:
+#         clear.pop(0)
+#     else:
+#         clear.pop()
+
+# print(clear)
 
 
 
